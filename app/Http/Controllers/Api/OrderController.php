b3060ab19a5e5ede747fe9641b4393f1180c6f31
nazaninns <nazaninnsq@gmail.com>
2023-12-05 01:03:37 +0330

    feat: add route for order in api

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
-    //
+    public function active()
+    {
+        $orders=Auth::user()->orders()->where('situation','!=','delivered')->get();
+        return response()->json(['data'=>$orders]);
+    }
+
+    public function deActive()
+    {
+        $orders=Auth::user()->orders()->where('situation','delivered')->get();
+        return response()->json(['data'=>$orders]);
+    }
+    public function show(Order $order)
+    {
+        return response()->json(['data'=>$order]);
+    }
 }
