2e21650514a39c7f7a452a6d46e75c93f6b47f9a
nazaninns <nazaninnsq@gmail.com>
2023-12-04 19:43:42 +0330

    fix: complete controller for time

--- app/Http/Controllers/seller/TimeController.php
+++ app/Http/Controllers/seller/TimeController.php
@@ -8,43 +8,65 @@ use App\Http\Requests\seller\time\UpdateRequest;
 use App\Models\Time;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class TimeController extends Controller
 {
     public function index()
     {
-        $times=Auth::user()->restaurant->times;
+        $times = Auth::user()->restaurant->times;
 
-        return view('seller.time',compact('times'));
+        return view('seller.time', compact('times'));
     }
 
-    public function update(Time $time,UpdateRequest $request)
+    public function update(Time $time, UpdateRequest $request)
     {
-        dd($request->validated());
+        $time->update($request->validated());
+        return redirect()->route('time.index');
     }
 
     public function close(Time $time)
     {
-        dd('bye bye hehe');
+        $time->update([
+            'start_time' => null,
+            'end_time' => null
+        ]);
+        return redirect()->route('time.index');
     }
 
-    public function someDay()
+    public function someDay(UpdateRequest $request)
     {
-
+        Auth::user()->restaurant->times()->where([
+            ['day', '!=', 'thursday'],
+            ['day', '!=', 'friday'],
+        ])->update($request->validated());
+        return redirect()->route('time.index');
     }
 
     public function someDayClose()
     {
-
+        Auth::user()->restaurant->times()->where([
+            ['day', '!=', 'thursday'],
+            ['day', '!=', 'friday'],
+        ])->update([
+            'start_time' => null,
+            'end_time' => null
+        ]);
+        return redirect()->route('time.index');
     }
 
-    public function allDay()
+    public function allDay(UpdateRequest $request)
     {
-
+        Auth::user()->restaurant->times()->update($request->validated());
+        return redirect()->route('time.index');
     }
 
     public function allDayClose()
     {
-
+        Auth::user()->restaurant->times()->update([
+            'start_time' => null,
+            'end_time' => null
+        ]);
+        return redirect()->route('time.index');
     }
 }
