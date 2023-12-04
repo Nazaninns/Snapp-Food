56c2d8c82b227497564581a02aec1e4be81e8d60
nazaninns <nazaninnsq@gmail.com>
2023-12-04 19:47:22 +0330

    feat: create policy for time

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -8,11 +8,13 @@ use App\Models\Cart;
 use App\Models\Comment;
 use App\Models\Order;
 use App\Models\Restaurant;
+use App\Models\Time;
 use App\Policies\AddressPolicy;
 use App\Policies\ArchivePolicy;
 use App\Policies\CartPolicy;
 use App\Policies\CommentPolicy;
 use App\Policies\RestaurantPolicy;
+use App\Policies\TimePolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -28,6 +30,7 @@ class AuthServiceProvider extends ServiceProvider
         Address::class=>AddressPolicy::class,
         Cart::class=>CartPolicy::class,
         Restaurant::class=>RestaurantPolicy::class,
+        Time::class=>TimePolicy::class
     ];
 
     /**
