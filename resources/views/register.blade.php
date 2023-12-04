0b2d21ed007af801245147067a3e705bdc56faa3
nazaninns <nazaninnsq@gmail.com>
2023-12-05 01:20:47 +0330

    feat: add password confirmation for web

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -115,6 +115,14 @@
                         </div>
 
                         <div class="mt-6">
+                            <div class="flex justify-between mb-2">
+                                <label for="password" class="text-sm text-gray-600 dark:text-gray-200"> confirmation Password</label>
+                            </div>
+
+                            <input type="password" name="password_confirmation" id="password" placeholder="Your Password"
+                                   class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"/>
+                        </div>
+                        <div class="mt-6">
                             <button
                                 class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
                                 Register
